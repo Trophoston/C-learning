506908895b150c7c527d8dbeea816387c862453c
Trophoston <95759773+Trophoston@users.noreply.github.com>
2023-06-08 21:13:45 +0700

    Com.w.1Octopus Drawing

---
+++ Octopus Drawing.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+    printf("Teachin Commahawong No.27 M.2/9\n\t\tGoon\n\n\n\n\n");
+    
+    //start shape
+    printf("            001110101               \n");
+    printf("          1010110101010             \n");
+    printf("        101000101011101101          \n");
+    printf("       10000101010111010000         \n");
+    printf("     010110         01100000        \n");
+    printf("     01001  >   <    11110001       \n");
+    printf("     1101             101000        \n");
+    printf("     01010    V        000110       \n");
+    printf("      0011011       001000010       \n");
+    printf("       010010100100010100000        \n");
+    printf("         10010000101010             \n");
+    printf("        1001010001010100            \n");
+    printf("       010000010010000000           \n");
+    printf("      100100101000100101011         \n");
+    printf("      1001 101000  100 10000        \n");
+    printf("       10    1000  10   10000       \n");
+    printf("       0     00    1    000         \n");
+    //น้องเป็นปลาหมึกแต่หนวดที่เหลือของน้องโดนบัง
+
+    return 0;
+}
+
