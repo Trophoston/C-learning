e02fa6060708ba0090aa53aa43f23ad903b7f0c3
Trophoston <95759773+Trophoston@users.noreply.github.com>
2023-06-08 21:11:31 +0700

    Com.w.0*Text

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+  printf("====Teachin Commahawong  No.27 M.2/9====\n====  Goon p.s.meaning pig in thai  ====");
+   // \n for make new  line
+
+  return 0;
+
+}
+
+
+
